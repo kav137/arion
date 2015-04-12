5cfa03570c7a733b3a63156270c5588c67332da0
kav <artyom.koval.137@gmail.com>
2015-04-12 12:48:00 -0600

    factories

--- js/app-tree.js
+++ js/app-tree.js
@@ -1,30 +1,38 @@
 angular.module('app-tree.service', [])
+	.factory('moduleFactory', function(){
+		return function createModule(modName){
+			var module = {"children": [], "moduleName": modName};
+			return module;
+		};
+	})
+	.factory('elementFactory', function(){
+		return function name(){
+			
+		};
+	})
 	.service('treeDataService', function(){
 		this.data = ['ready', 'set', 'go'];
 		var tree = [];
 		this.getTree = function () {
 			return angular.extend({}, tree);
 		}
-		this.pushItem = function (arg) {
+		this.pushNode = function (arg) {
 			tree.push(arg);
 		}
 	});
 
 angular.module('app-tree.controller', ['ngRoute'])
-	.controller('TreeCtrl', ['$scope', 'treeDataService', function ($scope, treeDataService){
+	.controller('TreeCtrl', ['$scope', 'treeDataService', 'moduleFactory', 
+	function ($scope, treeDataService, moduleFactory){
 		$scope.tree = treeDataService.getTree();
 		$scope.pushValue = function (arg) {
-			treeDataService.pushItem(arg);
+			treeDataService.pushNode(arg);
 			$scope.tree = treeDataService.getTree();
+			// var mod = moduleFactory.createModule(arg);
+			// console.log(mod);
+			console.log(moduleFactory(arg))
 		}
 	}]);
-
-/**
-* app-tree.service Module
-*
-* Description
-*/
-
 
 /**
 * app-tree Module
